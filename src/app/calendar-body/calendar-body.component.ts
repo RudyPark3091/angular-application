d5f365489e1c9304f3aae600981dc126351ab246
RudyPark3091 <grayblack313@gmail.com>
2021-03-04 00:14:04 +0900

    Add calendar view
    only displays current month

--- src/app/calendar-body/calendar-body.component.ts
+++ src/app/calendar-body/calendar-body.component.ts
@@ -7,13 +7,40 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class CalendarBodyComponent implements OnInit {
   @Input() date: Date;
+  firstDate: Date;
   days: number[] = [];
 
   constructor() {}
 
   ngOnInit(): void {
+    this.firstDate = new Date(this.date.getFullYear(), this.date.getMonth());
+    const day = this.firstDate.getDay();
+    const date = this.firstDate.getDate();
+    const lastDate = this.getLastDate(
+      this.date.getFullYear(),
+      this.date.getMonth()
+    );
+
     for (let i = 0; i < 42; i++) {
-      this.days.push(i);
+      if (i >= day && i < day + lastDate) {
+        this.days.push(i);
+      } else {
+        this.days.push(-1);
+      }
+    }
+  }
+
+  getLastDate(year: number, month: number) {
+    switch (month) {
+      case 1:
+        return year % 4 === 0 ? 29 : 28;
+      case 3:
+      case 5:
+      case 8:
+      case 10:
+        return 30;
+      default:
+        return 31;
     }
   }
 
